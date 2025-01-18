4ed8fca298b9d80b0ba3c38aa197495c8ca6c579
Ron Almog <ron8132@gmail.com>
2025-01-18 16:30:28 -0500

    add resumes page

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,5 +1,28 @@
-const ResumesPage = () => {
-  return <div>ResumesPage</div>;
+import { Button } from "@/components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { PlusSquare } from "lucide-react";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Your resumes",
+};
+
+const ResumesPage = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+  return (
+    <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
+      <Button asChild className="mx-auto flex w-fit gap-2">
+        <Link href="/editor">
+          <PlusSquare className="size-5" /> New resume
+        </Link>
+      </Button>
+    </main>
+  );
 };
 
 export default ResumesPage;
