08e36507d5558f5507dabaa5fd59a9c6ab97e565
Ron Almog <ron8132@gmail.com>
2025-01-22 17:04:41 -0500

    add skills to preview

--- src/components/resume-preview.tsx
+++ src/components/resume-preview.tsx
@@ -4,6 +4,8 @@ import { ResumeValues } from "@/lib/validation";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { formatDate } from "date-fns";
+import { BorderStyles } from "@/app/(main)/editor/border-style-button";
+import { Badge } from "./ui/badge";
 
 interface ResumePreviewProps {
   resumeData: ResumeValues;
@@ -34,6 +36,8 @@ export default function ResumePreview({
         <PersonalInfoHeader resumeData={resumeData} />
         <SummarySection resumeData={resumeData} />
         <WorkExperience resumeData={resumeData} />
+        <EducationSection resumeData={resumeData} />
+        <SkillsSection resumeData={resumeData} />
       </div>
     </div>
   );
@@ -135,6 +139,89 @@ function WorkExperience({ resumeData }: ResumeSectionProps) {
             <div className="whitespace-pre-line text-xs">{exp.description}</div>
           </div>
         ))}
+      </div>
+    </>
+  );
+}
+
+function EducationSection({ resumeData }: ResumeSectionProps) {
+  const { educations } = resumeData;
+
+  const educationsNotEmpty = educations?.filter(
+    (edu) => Object.values(edu).filter(Boolean).length > 0,
+  );
+  if (!educationsNotEmpty?.length) return null;
+
+  return (
+    <>
+      <hr className="border-2" />
+      <div className="space-y-3">
+        <p className="text-lg font-semibold">Education</p>
+        {educationsNotEmpty.map((edu, index) => (
+          <div key={index} className="break-inside-avoid space-y-1">
+            <div className="flex items-center justify-between text-sm font-semibold">
+              <span>{edu.degree}</span>
+              {edu.startDate && (
+                <span>
+                  {edu.startDate &&
+                    `${formatDate(edu.startDate, "MM/yyyy")}
+                  ${edu.endDate ? ` - ${formatDate(edu.endDate, "MM/yyyy")}` : ""}`}
+                </span>
+              )}
+            </div>
+            <p className="text-xs font-semibold">{edu.school}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+function SkillsSection({ resumeData }: ResumeSectionProps) {
+  const {
+    skills,
+    colorHex = "#050505",
+    borderStyle = BorderStyles.CIRCLE,
+  } = resumeData;
+
+  if (!skills?.length) return null;
+
+  return (
+    <>
+      <hr
+        className="border-2"
+        style={{
+          borderColor: colorHex,
+        }}
+      />
+      <div className="break-inside-avoid space-y-3">
+        <p
+          className="text-lg font-semibold"
+          style={{
+            color: colorHex,
+          }}
+        >
+          Skills
+        </p>
+        <div className="flex break-inside-avoid flex-wrap gap-2">
+          {skills.map((skill, index) => (
+            <Badge
+              key={index}
+              className="rounded-md bg-black text-white hover:bg-black"
+              style={{
+                backgroundColor: colorHex,
+                borderRadius:
+                  borderStyle === BorderStyles.SQUARE
+                    ? "0px"
+                    : borderStyle === BorderStyles.CIRCLE
+                      ? "9999px"
+                      : "8px",
+              }}
+            >
+              {skill}
+            </Badge>
+          ))}
+        </div>
       </div>
     </>
   );
