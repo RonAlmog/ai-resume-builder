73ba941dc210e85d0b4f0affe65f2588b1799ead
Ron Almog <ron8132@gmail.com>
2025-01-28 07:36:57 -0500

    add resumes page and preview

---
+++ src/app/(main)/resumes/resume-item.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import LoadingButton from "@/components/loading-button";
+import ResumePreview from "@/components/resume-preview";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useToast } from "@/hooks/use-toast";
+import { ResumeServerData } from "@/lib/types";
+import { mapToResumeValues } from "@/lib/utils";
+import { formatDate } from "date-fns";
+import { MoreVertical, Printer, Trash2 } from "lucide-react";
+import Link from "next/link";
+import { useRef, useState, useTransition } from "react";
+
+interface ResumeItemProps {
+  resume: ResumeServerData;
+}
+
+export default function ResumeItem({ resume }: ResumeItemProps) {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const wasUpdated = resume.updatedAt !== resume.createdAt;
+
+  return (
+    <div className="group rounded-lg border border-transparent bg-secondary p-2 transition-colors hover:border-border">
+      <div className="space-y-3">
+        <Link
+          href={`/editor?resumeId=${resume.id}`}
+          className="inline-block w-full text-center"
+        >
+          <p className="line-clamp-1 font-semibold">
+            {resume.title || "No title"}
+          </p>
+          {resume.description && (
+            <p className="line-clamp-2 text-sm">{resume.description}</p>
+          )}
+          <p className="text-xs text-muted-foreground">
+            {wasUpdated ? "Updated" : "Created"} on{" "}
+            {formatDate(resume.updatedAt, "MMM d, yyyy")}
+          </p>
+        </Link>
+        <Link
+          href={`/editor?resumeId=${resume.id}`}
+          className="inline-block w-full"
+        >
+          <ResumePreview
+            resumeData={mapToResumeValues(resume)}
+            className="shadow-sm transition-shadow group-hover:shadow-lg"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+}
