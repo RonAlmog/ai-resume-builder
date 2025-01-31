6884988a90ffb70adf5797281f7e28b5448e4e29
Ron Almog <ron8132@gmail.com>
2025-01-31 08:14:13 -0500

    add stripe create session action

---
+++ src/app/actions/create-checkout-session.ts
@@ -0,0 +1,51 @@
+"use server";
+
+// import { env } from "@/env";
+import stripe from "@/lib/stripe";
+import { currentUser } from "@clerk/nextjs/server";
+
+export async function createCheckoutSession(priceId: string) {
+  const user = await currentUser(); // different from auth. returns the whole user object.
+
+  if (!user) {
+    throw new Error("Unauthorized");
+  }
+
+  const stripeCustomerId = user.privateMetadata.stripeCustomerId as
+    | string
+    | undefined;
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [{ price: priceId, quantity: 1 }],
+    mode: "subscription",
+    // when done, stripe will redirect to one of these urls
+    success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/billing/success`,
+    cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/billing`,
+    customer: stripeCustomerId,
+    customer_email: stripeCustomerId
+      ? undefined
+      : user.emailAddresses[0].emailAddress,
+    metadata: {
+      userId: user.id,
+    },
+    subscription_data: {
+      metadata: {
+        userId: user.id,
+      },
+    },
+    custom_text: {
+      terms_of_service_acceptance: {
+        message: `I have read AI Resume Builder's [terms of service](${process.env.NEXT_PUBLIC_BASE_URL}/tos) and agree to them.`,
+      },
+    },
+    consent_collection: {
+      terms_of_service: "required",
+    },
+  });
+
+  if (!session.url) {
+    throw new Error("Failed to create checkout session");
+  }
+
+  return session.url;
+}
