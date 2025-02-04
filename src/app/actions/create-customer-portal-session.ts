d2c4d44a18e04722ccb8a7b9f4601878bbc4fc03
Ron Almog <ron8132@gmail.com>
2025-02-04 08:43:40 -0500

    add action for stripe portal

---
+++ src/app/actions/create-customer-portal-session.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { env } from "@/env";
+import stripe from "@/lib/stripe";
+import { currentUser } from "@clerk/nextjs/server";
+
+// this action basically calls stripe with our stripeCustomerId
+// and returns the url to the customer portal
+export async function createCustomerPortalSession() {
+  const user = await currentUser();
+
+  if (!user) {
+    throw new Error("Unauthorized");
+  }
+
+  // stripeCustomerId is our own data, stored in the user's private metadata
+  const stripeCustomerId = user.privateMetadata.stripeCustomerId as
+    | string
+    | undefined;
+
+  if (!stripeCustomerId) {
+    throw new Error("Stripe customer ID not found");
+  }
+
+  const session = await stripe.billingPortal.sessions.create({
+    customer: stripeCustomerId,
+    return_url: `${env.NEXT_PUBLIC_BASE_URL}/billing`,
+  });
+
+  if (!session.url) {
+    throw new Error("Failed to create customer portal session");
+  }
+
+  return session.url;
+}
